61c6c9941f3c9948a1c61a7fcc1ade5010719939
enderjj <chnhongjj@163.com>
2019-04-21 21:01:48 +0800

    feat: swiper component

--- travel/src/main.js
+++ travel/src/main.js
@@ -7,9 +7,13 @@ import 'styles/reset.css' // 初始化样式
 import 'styles/border.css' // 解决移动端 border 1px 问题
 import 'styles/iconfont.css' // 引入图标样式
 import fastClick from 'fastclick' // 解决移动端点击事件的 300 ms 延迟问题
+import VueAwesomeSwiper from 'vue-awesome-swiper' // 引入轮播图插件
+import 'swiper/dist/css/swiper.css' // 轮播图插件对应的样式
 
 Vue.config.productionTip = false
 fastClick.attach(document.body) // 启用 fastclick
+
+Vue.use(VueAwesomeSwiper) // 在 Vue 中使用 vue-awesome-swiper 插件
 
 /* eslint-disable no-new */
 new Vue({
---
+++ travel/src/pages/components/swiper.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides 轮播页面 -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <!-- Optional controls 轮播图其他选项 -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/389d0194766566f8f1185aa19b759c25.jpg_750x200_22620261.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/e5104d77fbfdb522bd4fe4f0451e517f.jpg_750x200_7d44f64b.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  // 下面两行是为了消除 scoped 的影响，设置轮播图圆点的样式，表示 .wrapper 下面的所有 .swiper-pagination-bullet-active 元素都设置这个样式
+  .wrapper >>> .swiper-pagination-bullet-active
+    background-color: #fff
+  .wrapper
+    // 下面 4 行样式设置是为了防止图片下方的文字出现抖动现象
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 26.7% // 表示相对于宽度的 26.7%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
