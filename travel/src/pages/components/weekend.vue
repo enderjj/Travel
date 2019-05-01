916528f5f1d9b85a3cc1ef9076ccb12ffc3bc5e0
enderjj <chnhongjj@163.com>
2019-05-01 17:54:40 +0800

    feat: weekend component

---
+++ travel/src/pages/components/weekend.vue
@@ -0,0 +1,74 @@
+// 周末去哪儿部分组件
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend', // 组件名称
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/4e/3e87ee62e59cdb.jpg_r_640x214_fe6cc902.jpg',
+          title: '西溪国家湿地公园',
+          desc: '浪漫的湿地公园，风景美不胜收'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/4e/3e87ee62e59cdb.jpg_r_640x214_fe6cc902.jpg',
+          title: '西溪国家湿地公园',
+          desc: '浪漫的湿地公园，风景美不胜收'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/4e/3e87ee62e59cdb.jpg_r_640x214_fe6cc902.jpg',
+          title: '西溪国家湿地公园',
+          desc: '浪漫的湿地公园，风景美不胜收'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "~styles/mixin.styl";
+
+  .title
+    line-height: .8rem
+    background: #eee
+    font-size: .32rem
+    text-indent: .2rem
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 33.9%
+    .item-img
+      width: 100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      font-size: .26rem
+      color: #ccc
+      ellipsis()
+</style>
--- travel/src/pages/home.vue
+++ travel/src/pages/home.vue
@@ -5,6 +5,7 @@
     <home-swiper></home-swiper>
     <home-icons></home-icons>
     <home-recommend></home-recommend>
+    <home-weekend></home-weekend>
   </div>
 </template>
 
@@ -13,13 +14,15 @@ import HomeHeader from './components/header'
 import HomeSwiper from './components/swiper'
 import HomeIcons from './components/travelicons'
 import HomeRecommend from './components/recommend'
+import HomeWeekend from './components/weekend'
 
 export default {
   components: {
     HomeHeader,
     HomeSwiper,
     HomeIcons,
-    HomeRecommend
+    HomeRecommend,
+    HomeWeekend
   }
 }
 </script>
