5fe5ecde08a9f29dee30efb25be3efa09cb72700
enderjj <chnhongjj@163.com>
2019-05-11 16:25:53 +0800

    feat: Recursive component

---
+++ travel/src/pages/detail/components/list.vue
@@ -0,0 +1,44 @@
+<template>
+  <ul>
+    <li v-for="(item, index) of list" :key="index">
+      <p class="item-title border-bottom">
+        <span class="item-icon"></span>
+        {{item.title}}
+      </p>
+      <!-- 使用递归组件显示层级内容 组件调用自身组件 -->
+      <div class="item-children" v-if="item.children">
+        <detail-list :list="item.children"></detail-list>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  props: {
+    list: {
+      type: Array
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.item-icon
+  display: inline-block
+  position: relative
+  width: .36rem
+  height: .36rem
+  top: .08rem
+  left: .1rem
+  background: url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat
+  margin-right: .1rem
+  background-size: .4rem 3rem
+.item-title
+  line-height: .8rem
+  font-size: .32rem
+  padding-left: .2rem
+.item-children
+  padding-left: .2rem
+</style>
--- travel/src/pages/detail/detail.vue
+++ travel/src/pages/detail/detail.vue
@@ -3,19 +3,54 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import detailBanner from './components/banner'
 import DetailHeader from './components/header'
+import DetailList from './components/list'
 
 export default {
   name: 'Detail',
   components: {
     detailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [
+        {
+          title: '成人票',
+          children: [
+            {
+              title: '成人票三馆联票'
+            },
+            {
+              title: '成人票五馆联票',
+              children: [
+                {
+                  title: '三馆联票-杭州销售'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          title: '学生票'
+        },
+        {
+          title: '儿童票'
+        },
+        {
+          title: '特惠票'
+        }
+      ]
+    }
   }
 }
 </script>
